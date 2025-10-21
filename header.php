39ae47e7b8ecb9e2c08a40401cbd98ae5169edc5
Bavar <bavarbavar.net@gmail.com>
2025-10-21 19:04:36 +0330

    two

--- header.php
+++ header.php
@@ -5,4 +5,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Legacy Weblog</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+    <link rel="stylesheet" href="style/style.css">
   </head>
